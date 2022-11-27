ba110bf131d0916234e5b3c38e4c7dd80b90a275
Tobias Bengtsson <me@tobiasbengtsson.se>
2022-11-27 13:16:12 +0100

    Correct test comment

--- src/lib.rs
+++ src/lib.rs
@@ -164,7 +164,7 @@ mod tests {
 
     #[test]
     pub fn test_lorem() {
-        // Lorem ipsum padded to 128-bits
+        // Lorem ipsum
         let result = hash_raw(b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing");
         assert_eq!(result, 0x470B16);
     }
